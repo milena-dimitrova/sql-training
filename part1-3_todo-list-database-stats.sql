f497cdfff48c228b5a67b40f17dbcb4d37047070
Milena Dimitrova <53831497+milena-dimitrova@users.noreply.github.com>
2019-08-13 08:56:07 +0100

    Create part1-3_todo-list-database-stats.sql

---
+++ part1-3_todo-list-database-stats.sql
@@ -0,0 +1,5 @@
+/*https://www.khanacademy.org/computing/computer-programming/sql/sql-basics/pc/challenge-todo-list-database-stats*/
+
+INSERT INTO todo_list VALUES (4, "Eat", 45);
+
+SELECT SUM(minutes) FROM todo_list;
