1f8d0b7e712bf07c64961eb0b0ebd85728021235
Milena Dimitrova <53831497+milena-dimitrova@users.noreply.github.com>
2019-08-13 08:57:08 +0100

    Create part1-2_box-office-hits-database.sql

---
+++ part1-2_box-office-hits-database.sql
@@ -0,0 +1,4 @@
+/*https://www.khanacademy.org/computing/computer-programming/sql/sql-basics/pc/challenge-box-office-hits-database*/
+
+SELECT * FROM movies;
+SELECT * FROM movies WHERE release_year >= 2000 ORDER BY release_year;
